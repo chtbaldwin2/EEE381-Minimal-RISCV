99cee563050847235a449d7df8677d4225ecb54f
chtbaldwin2 <101398947+chtbaldwin2@users.noreply.github.com>
2023-02-22 12:03:16 +0000

    Updated reg file to include comments

--- reg/riscv_regfile.sv
+++ reg/riscv_regfile.sv
@@ -1,27 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.02.2023 13:20:20
-// Design Name: 
-// Module Name: riscv_regfile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
+//register file for RISC-V CPU
 module riscv_regfile(
 
+        //define inputs and outputs
         input clk,
         input [4:0] readReg1,
         input [4:0] readReg2,
@@ -33,22 +15,32 @@ module riscv_regfile(
 
     );
     
+    //create register to store data
+    //32 bit register - RV32I
     logic [31:0] register[31:0];
     
+    //fill register with all zeros - ready for data to be loaded in
     integer i;
     initial
         for (i=0; i<32; i=i+1)
             register[i] = 0;
     
+    //Able to read two registers and write to one in a single clock cycle
+    //multi-ported memory
     always_ff@(posedge clk) begin
-        readData1 <= register[readReg1];
-        readData2 <= register[readReg2];
-        if ((write) && (writeReg!=0)) begin
-            register[writeReg] <= writeData;
-            if (readReg1 == writeReg)
+    
+        readData1 <= register[readReg1];        //read data from reg1
+        readData2 <= register[readReg2];        //read data from reg2
+        
+        if ((write) && (writeReg!=0)) begin     //ensure that the CPU is ready to write
+                                                //and the register being written to isn't x0
+            register[writeReg] <= writeData;    //write data into the register
+            
+            if (readReg1 == writeReg)           //data is updated
                 readData1 <= writeData;
-            if (readReg2 == writeReg)
+            if (readReg2 == writeReg)           
                 readData2 <= writeData;
+                
         end
     end
     
